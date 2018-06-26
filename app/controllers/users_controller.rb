31ec1ad1a18aee8c4d16ac5eb83125f1c75b5cd1
Ryan <rdo9313@gmail.com>
2018-06-25 23:20:32 -0400

    posting signup params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :'/users/signup'
   end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    else
+      User.create(username: params[:username], email: params[:email], password: params[:password])
+      redirect '/users/show'
+    end
+  end
 end
