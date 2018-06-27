069b11562c92c4c6ca0dbe9bab211094a87fc395
Ryan <rdo9313@gmail.com>
2018-06-27 18:19:35 -0400

    added balls edit page and patch route

--- app/controllers/balls_controller.rb
+++ app/controllers/balls_controller.rb
@@ -17,11 +17,31 @@ class BallsController < ApplicationController
     erb :'/balls/show'
   end
 
+  get '/balls/:id/edit' do
+    @ball = Ball.find(params[:id])
+    erb :'/balls/edit'
+  end
+
   post '/balls' do
     if params[:name].empty? || params[:brand].empty? || params[:color].empty? || params[:weight].empty? || params[:coverstock].empty? || params[:release_date].empty?
       redirect '/balls/new'
     else
       current_user.balls.create(name: params[:name], brand: params[:brand], color: params[:color], weight: params[:weight], coverstock: params[:coverstock], release_date: params[:release_date])
+      redirect '/balls'
+    end
+  end
+
+  patch '/balls/:id/edit' do
+    @ball = Ball.find(params[:id])
+    if current_user == @ball.user
+      if params[:name].empty? || params[:brand].empty? || params[:color].empty? || params[:weight].empty? || params[:coverstock].empty? || params[:release_date].empty?
+        redirect "/balls/#{@ball.id}/edit"
+      else
+        @ball.update(name: params[:name], brand: params[:brand], color: params[:color], weight: params[:weight], coverstock: params[:coverstock], release_date: params[:release_date])
+        @ball.save
+        redirect '/balls'
+      end
+    else
       redirect '/balls'
     end
   end
